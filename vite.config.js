4b09ef92a98707f6076eee2e85ab9ebcda6f96cf
jkwon240 <jy7065@gmail.com>
2025-06-05 18:47:58 +0900

    🎉 프로젝트 초기 커밋

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/project04_game/", // ✅ 리포지토리 이름을 정확히 입력!
+  base: "/jkwon_first_project/", // ✅ 리포지토리 이름을 정확히 입력!
   plugins: [react()],
 });
